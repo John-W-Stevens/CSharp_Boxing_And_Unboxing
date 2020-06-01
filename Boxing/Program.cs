38967981cfdb0d0dbedea22d8f4b5e001d197e4c
John Stevens <john.william.stevens1@gmail.com>
2020-06-01 15:06:24 -0700

    initial commit

---
+++ Boxing/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an empty List of type object
+            List<object> BoxedData = new List<object>();
+
+            // Add the following values to the list: 7, 28, -1, true, "chair"
+            BoxedData.Add(7);
+            BoxedData.Add(28);
+            BoxedData.Add(-1);
+            BoxedData.Add(true);
+            BoxedData.Add("chair");
+
+            // Loop through the list and print all values (Hint: Type Inference might help here!)
+            foreach (var Value in BoxedData)
+            {
+                Console.WriteLine(Value);
+            }
+
+            // Add all values that are Int type together and output the sum
+            int Sum = 0;
+            foreach (var Value in BoxedData)
+            {
+                if (Value is int)
+                {
+                    int ExplicitValue = (int)Value;
+                    Sum += ExplicitValue;
+                }
+            }
+            Console.WriteLine(Sum); // Output -> 34 (28 + 7 - 1 = 34)
+        }
+    }
+}
